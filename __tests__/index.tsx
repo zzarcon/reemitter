f9d8057689dbf313b71f55113b13cdc72199b7c6
Hector Leon Zarco Garcia <hezarco@gmail.com>
2019-06-12 21:51:01 +1000

    Implement off + use Set()

--- __tests__/index.tsx
+++ __tests__/index.tsx
@@ -1,20 +1,52 @@
 import Reemitter from "../src";
 
 describe('Reemitter', () => {
-  it('Should call registered events', () => {
+  it('should call registered events', () => {
     const reemitter = new Reemitter<{foo: {bar: number}}>();
     const fooListener = jest.fn();
     const secondListener = jest.fn();
 
     reemitter.on('foo', fooListener);
     reemitter.on('foo', secondListener);
-    reemitter.on('foo', secondListener);
     reemitter.emit('foo', {bar: 1});
 
     expect(fooListener).toBeCalledTimes(1);
     expect(fooListener).toBeCalledWith({bar: 1});
 
-    expect(secondListener).toBeCalledTimes(2);
+    expect(secondListener).toBeCalledTimes(1);
     expect(secondListener).toBeCalledWith({bar: 1});
   });
+
+  it('should not call unregistered events', () => {
+    const reemitter = new Reemitter<{foo: {bar: number}, hector: number}>();
+    const fooListener = jest.fn();
+
+    reemitter.on('foo', fooListener);
+    reemitter.emit('hector', 1);
+
+    expect(fooListener).not.toHaveBeenCalled();
+  });
+
+  it('should call listener once when registered multiple times', () => {
+    const reemitter = new Reemitter<{foo: {bar: number}}>();
+    const fooListener = jest.fn();
+
+    reemitter.on('foo', fooListener);
+    reemitter.on('foo', fooListener);
+    reemitter.on('foo', fooListener);
+    reemitter.emit('foo', {bar: 1});
+
+    expect(fooListener).toBeCalledTimes(1);
+  });
+
+  it('should allow to unsubscribe from listeners', () => {
+    const reemitter = new Reemitter<{foo: {bar: number}}>();
+    const fooListener = jest.fn();
+
+    reemitter.on('foo', fooListener);
+    reemitter.off('foo', fooListener);
+    reemitter.emit('foo', {bar: 1});
+
+    expect(fooListener).not.toHaveBeenCalled()
+  })
 });
--- src/reemitter.ts
+++ src/reemitter.ts
@@ -9,28 +9,30 @@ export type EventPayloadListener<
 > = (payload: M[E]) => void;
 
 export class Reemitter<M> {
-  //{[key in keyof M]: Function[]};
   private readonly listeners: {
-    [E in keyof M]?: EventPayloadListener<M, E>[];
+    [E in keyof M]?: Set<EventPayloadListener<M, E>>;
   };
-  // private readonly listeners: Map<keyof M, EventPayloadListener<M, E>[]>
 
   constructor() {
     this.listeners = {};
   }
 
   on<E extends keyof M>(event: E, listener: EventPayloadListener<M, E>): void {
-    const existingListeners = this.listeners[event] || [];
-    existingListeners.push(listener);
+    const existingListeners = this.listeners[event] || new Set();
+    existingListeners.add(listener);
     this.listeners[event] = existingListeners;
   }
 
   off<E extends keyof M>(event: E, listener: EventPayloadListener<M, E>): void {
-    
+    const existingListeners = this.listeners[event];
+
+    if (existingListeners) {
+      existingListeners.delete(listener);
+    }
   }
 
   emit<E extends keyof M>(event: E, payload: M[E]): boolean {
-    const existingListeners = this.listeners[event]
+    const existingListeners = this.listeners[event];
 
     if (existingListeners) {
       existingListeners.forEach(listener => listener(payload));
